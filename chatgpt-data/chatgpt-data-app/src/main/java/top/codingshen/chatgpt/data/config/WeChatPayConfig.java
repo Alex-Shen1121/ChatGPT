0409eefdadee593978ca0084d32cd1258cb55b07
AlexShen <73343630+Alex-Shen1121@users.noreply.github.com>
2024-01-03 21:36:14 +0800

    feat: 微信支付配置

---
+++ chatgpt-data/chatgpt-data-app/src/main/java/top/codingshen/chatgpt/data/config/WeChatPayConfig.java
@@ -0,0 +1,109 @@
+package top.codingshen.chatgpt.data.config;
+
+import com.wechat.pay.java.core.Config;
+import com.wechat.pay.java.core.RSAAutoCertificateConfig;
+import com.wechat.pay.java.core.notification.NotificationConfig;
+import com.wechat.pay.java.core.notification.NotificationParser;
+import com.wechat.pay.java.service.payments.nativepay.NativePayService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.File;
+import java.io.InputStream;
+
+/**
+ * @ClassName WeChatPayConfig
+ * @Description description
+ * @Author alex_shen
+ * @Date 2024/1/3 - 18:20
+ */
+@Slf4j
+@Configuration
+@EnableConfigurationProperties(WeChatPayConfigProperties.class)
+public class WeChatPayConfig {
+
+    /**
+     * 一个商户号只能初始化一个配置，否则会因为重复的下载任务报错
+     *
+     * @param properties 支付配置
+     * @return NativePay
+     */
+    @Bean
+    public NativePayService buildNativePayService(WeChatPayConfigProperties properties) {
+        // 支付配置
+        Config config = new RSAAutoCertificateConfig.Builder()
+                .merchantId(properties.getMchid())
+                .privateKeyFromPath(getFilePath(properties.getPrivateKeyPath()))
+                .merchantSerialNumber(properties.getMerchantSerialNumber())
+                .apiV3Key(properties.getApiV3Key())
+                .build();
+
+        // NativePay 支付服务
+        return new NativePayService.Builder().config(config).build();
+    }
+
+    @Bean
+    public NotificationConfig buildNotificationConfig(WeChatPayConfigProperties properties) {
+        return new RSAAutoCertificateConfig.Builder()
+                .merchantId(properties.getMchid())
+                .privateKeyFromPath(getFilePath(properties.getPrivateKeyPath()))
+                .merchantSerialNumber(properties.getMerchantSerialNumber())
+                .apiV3Key(properties.getApiV3Key())
+                .build();
+    }
+
+    @Bean
+    public NotificationParser buildNotificationParser(NotificationConfig notificationConfig) {
+        return new NotificationParser(notificationConfig);
+    }
+
+    public static String getFilePath(String classFilePath) {
+        String filePath = "";
+        try {
+            String templateFilePath = "tempfiles/classpathfile/";
+            File tempDir = new File(templateFilePath);
+            if (!tempDir.exists()) {
+                tempDir.mkdirs();
+            }
+            String[] filePathList = classFilePath.split("/");
+            String checkFilePath = "tempfiles/classpathfile";
+            for (String item : filePathList) {
+                checkFilePath += "/" + item;
+            }
+            File tempFile = new File(checkFilePath);
+            if (tempFile.exists()) {
+                filePath = checkFilePath;
+            } else {
+                //解析
+                ClassPathResource classPathResource = new ClassPathResource(classFilePath);
+                InputStream inputStream = classPathResource.getInputStream();
+                checkFilePath = "tempfiles/classpathfile";
+                for (int i = 0; i < filePathList.length; i++) {
+                    checkFilePath += "/" + filePathList[i];
+                    if (i == filePathList.length - 1) {
+                        //文件
+                        File file = new File(checkFilePath);
+                        if (!file.exists()) {
+                            FileUtils.copyInputStreamToFile(inputStream, file);
+                        }
+                    } else {
+                        //目录
+                        tempDir = new File(checkFilePath);
+                        if (!tempDir.exists()) {
+                            tempDir.mkdirs();
+                        }
+                    }
+                }
+                inputStream.close();
+                filePath = checkFilePath;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return filePath;
+    }
+}
---
+++ chatgpt-data/chatgpt-data-app/src/main/java/top/codingshen/chatgpt/data/config/WeChatPayConfigProperties.java
@@ -0,0 +1,40 @@
+package top.codingshen.chatgpt.data.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @ClassName WeChatPayConfigProperties
+ * @Description 微信支付配置
+ * @Author alex_shen
+ * @Date 2024/1/3 - 18:20
+ */
+@Data
+@ConfigurationProperties(prefix = "wxpay.config", ignoreInvalidFields = true)
+public class WeChatPayConfigProperties {
+
+    /**
+     * 申请支付主体的 appid
+     */
+    private String appid;
+    /**
+     * 商户号
+     */
+    private String mchid;
+    /**
+     * 回调地址
+     */
+    private String notifyUrl;
+    /**
+     * 商户API私钥路径
+     */
+    private String privateKeyPath;
+    /**
+     * 商户证书序列号：openssl x509 -in apiclient_cert.pem -noout -serial
+     */
+    private String merchantSerialNumber;
+    /**
+     * 商户APIV3密钥
+     */
+    private String apiV3Key;
+}
